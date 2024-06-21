60514f4c58f4c413476b1404122f692555c1e704
harry <harrytangwin@yans-imac.hsd1.ca.comcast.net>
2024-06-20 20:06:54 -0700

    add getPossibleMoves() for King

--- src/King.java
+++ src/King.java
@@ -1,20 +1,34 @@
 import java.util.*;
 
-
 public class King extends Piece {
 
 	public King(PColor c, Position p) {
 		super(c, p);
 	}
-	
+
 	public List<Position> getPossibleMoves(Board board) {
-		// TODO
-		return new ArrayList<Position>();
+		List<Position> pms = new ArrayList<Position>();
+		if (!this.isAlive)
+			return pms;
+
+		for (int x : new int[] { -1, 0, 1 }) {
+			for (int y : new int[] { -1, 0, 1 }) {
+				Position newPos = this.position.add(x, y);
+				if (newPos != null) {
+					Piece p = board.getPiece(newPos);
+					if (p == null) {
+						pms.add(newPos);
+					} else if (p.color != this.color) {
+						pms.add(newPos);
+					}
+				}
+			}
+		}
+		return pms;
 	}
-	
+
 	public String toString() {
 		return (this.color == PColor.WHITE ? "♔" : "♚") + this.position;
 	}
-	
-	
+
 }
